8d127cafade94908f5b58194cf6b19995e2a1009
Daryl Wada <daryl.wada@gmail.com>
2018-06-08 09:43:34 -0700

    Demonstrates how to re-render based on a hash change.

---
+++ 20180608-hashchange/main.js
@@ -0,0 +1,23 @@
+/* global state, queryString, renderPokedex */
+
+renderApp(state)
+
+function renderApp(state) {
+  var $app = document.querySelector('#app')
+  $app.innerHTML = ''
+  $app.appendChild(renderPokedex(state.pokedex, state.type))
+}
+
+/**
+ * Add your 'hashchange' event listener here.
+ *
+ */
+
+window.addEventListener('hashchange', (event) => {
+  var hash = location.hash
+  var type = queryString.parse(hash)['?type']
+  state.type = type
+  renderApp(state)
+})
+
+window.dispatchEvent(new Event('hashchange'))
