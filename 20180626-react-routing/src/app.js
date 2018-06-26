4785540bb1887b4974bbc26e9ebfba6bcd049bd7
Daryl Wada <daryl.wada@gmail.com>
2018-06-26 10:38:03 -0700

    Completed React Routing warm up.

---
+++ 20180626-react-routing/src/app.js
@@ -0,0 +1,42 @@
+import React, { Fragment, Component } from 'react'
+import Nav from './nav'
+import Pokedex from './pokedex'
+import * as queryString from './query-string'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      type: ''
+    }
+  }
+  componentDidMount() {
+
+    /**
+     * Your code here
+     */
+
+    window.addEventListener('hashchange', event => {
+      const hash = location.hash
+      const type = queryString.parse(hash.slice(1)).type
+      this.setState({ type: type })
+    })
+
+  }
+  render() {
+    const { type } = this.state
+    const { pokedex } = this.props
+    const activePokemon = type
+      ? pokedex.filter(pokemon => pokemon.type === type)
+      : pokedex
+    return (
+      <Fragment>
+        <h1 className="text-center">Pokedex</h1>
+        <div className="container">
+          <Nav activeType={type}/>
+          <Pokedex pokedex={activePokemon}/>
+        </div>
+      </Fragment>
+    )
+  }
+}
