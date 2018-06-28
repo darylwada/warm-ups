7f586760d2a21f0e852d934328e2391a6e3c51e1
Daryl Wada <daryl.wada@gmail.com>
2018-06-28 13:28:38 -0700

    Cleaned up solution and made it more robust.

--- 20180627-deep-copy/src/main.js
+++ 20180627-deep-copy/src/main.js
@@ -9,32 +9,15 @@ function deepCopy(input) {
   }
 
   if (input instanceof Array) {
-
-    if (input.every(element => element instanceof Date)) {
-      return input.map(element => new Date(element))
-    }
-
-    if (input.every(element => element instanceof Object && !(element instanceof Array))) {
-      const shallowCopy = input.map(element => Object.assign({}, element))
-      const deepCopy = shallowCopy.map(element => {
-        for (let key in element) {
-          let value = element[key]
-          if (value instanceof Date) {
-            value = new Date(value)
-            element[key] = value
-            return element
-          }
-        }
-        return element
-      })
-      return deepCopy
-    }
-
-    return [...input]
+    return input.map(element => deepCopy(element))
   }
 
   if (input instanceof Object) {
-    return Object.assign({}, input)
+    const copy = {}
+    for (let key in input) {
+      copy[key] = deepCopy(input[key])
+    }
+    return copy
   }
 
 }
