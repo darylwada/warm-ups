c483c0648f6fa8823f488733bffbdc495066482b
Daryl Wada <daryl.wada@gmail.com>
2018-06-28 10:10:31 -0700

    Completed without using JSON.stringify.

--- 20180628-deep-equals/src/main.js
+++ 20180628-deep-equals/src/main.js
@@ -1,5 +1,26 @@
 function deepEquals(x, y) {
+
   if (!(x instanceof Object) && !(y instanceof Object)) return x === y
-  if (x instanceof Object && y instanceof Object) return JSON.stringify(x) === JSON.stringify(y)
+
+  if (x instanceof Date && y instanceof Date) return x.toJSON() === y.toJSON()
+
+  if (Array.isArray(x) && Array.isArray(y)) {
+    return x.length === y.length
+      ? x.every((element, i) => deepEquals(element, y[i]))
+      : false
+  }
+
+  if (x instanceof Object && y instanceof Object) {
+    const xEntries = Object.entries(x)
+    const yEntries = Object.entries(y)
+
+    const keysEqual = xEntries.length === yEntries.length
+      ? xEntries.every((pair, i) => pair[0] === yEntries[i][0])
+      : false
+    const valuesEqual = xEntries.every((pair, i) => deepEquals(pair[1], yEntries[i][1]))
+
+    return keysEqual && valuesEqual
+  }
+
   return false
 }
